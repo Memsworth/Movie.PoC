433d7fa885f24ac0a77836e43fb9147557735ba6
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2024-02-07 01:35:32 +0300

    added film service to check if data is being sent back

---
+++ Movie.PoC.Api/Controllers/FilmController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie.PoC.Api.Features.Films;
+
+namespace Movie.PoC.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FilmController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMovieData(string request)
+        {
+            var query = new FilmService.GetFilmDataQuery(request);
+            var result = await _mediator.Send(query);
+            return result is not null ? Ok(request) : BadRequest();
+        }
+    }
+}
---
+++ Movie.PoC.Api/Features/Films/FilmService.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Movie.PoC.Api.Entities;
+using System.Text.Json;
+
+namespace Movie.PoC.Api.Features.Films
+{
+    public static class FilmService
+    {
+        public record GetFilmDataQuery(string ImdbId) : IRequest<FilmDataRaw?>;
+
+        internal sealed class GetFilmDataHandler : IRequestHandler<GetFilmDataQuery, FilmDataRaw?>
+        {
+            private readonly HttpClient _httpClient;
+
+            public GetFilmDataHandler() => _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.omdbapi.com/")
+            };
+
+
+            public async Task<FilmDataRaw?> Handle(GetFilmDataQuery request, CancellationToken cancellationToken)
+            {
+                string url = $"?i={request.ImdbId}&apikey=66b0c81f".ToString();
+
+                var response = await _httpClient.GetAsync(url, cancellationToken);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                var jsonFile = await response.Content.ReadAsStreamAsync(cancellationToken);
+                var jsonContent = await JsonSerializer.DeserializeAsync<FilmDataRaw>(jsonFile, cancellationToken: cancellationToken);
+                return jsonContent;
+            }
+        }
+    }
+}
