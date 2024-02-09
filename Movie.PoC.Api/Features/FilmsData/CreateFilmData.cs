63193423c3e8beabaadf09161bfb009562c65e88
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2024-02-09 17:49:14 +0300

    Create CreateFilmData.cs

---
+++ Movie.PoC.Api/Features/FilmsData/CreateFilmData.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Movie.PoC.Api.Features.FilmsData
+{
+    public record CreateFilmDataCommand(string ImdbId) : IRequest
+    {
+    }
+
+    public class CreateFilmDataCommandHandler : IRequestHandler<CreateFilmDataCommand>
+    {
+
+    }
+
+
+    public class CreateFilmDataCommandValidator : AbstractValidator<CreateFilmDataCommand>
+    {
+        public CreateFilmDataCommandValidator()
+        {
+            RuleFor(request => request.ImdbId)
+                .NotEmpty()
+                .WithMessage("The ID can't be empty. Please provide an movie id");
+        }
+    }
+}
